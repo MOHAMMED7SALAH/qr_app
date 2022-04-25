866a3e7ad48825b9a934b50117d19cb65601bdd6
MOHAMMED7SALAH <bentitimohammedsalahstars7@gmail.com>
2022-04-25 08:23:04 +0200

    qr_code_app

---
+++ lib/page/qr_create_page.dart
@@ -0,0 +1,108 @@
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:davinci/davinci.dart';
+import 'package:qr_code_scanner_example/main.dart';
+
+class QRCreatePage extends StatefulWidget {
+  @override
+  _QRCreatePageState createState() => _QRCreatePageState();
+}
+
+class _QRCreatePageState extends State<QRCreatePage> {
+  final controller = TextEditingController();
+
+  GlobalKey imageKey;
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Davinci(builder: (key) {
+                  imageKey = key;
+                  return BarcodeWidget(
+                    barcode: Barcode.qrCode(),
+                    color: Colors.white,
+                    data: controller.text ?? 'Hello world',
+                    width: 200,
+                    height: 200,
+                  );
+                }),
+                SizedBox(height: 40),
+                ElevatedButton(
+                    onPressed: () async {
+                      Get.showSnackbar(const GetSnackBar(
+                        titleText: Center(
+                            child: Text('Congratulation 🥳🎉',
+                                style: TextStyle(color: Colors.white))),
+                        messageText: Center(
+                          child: Text(
+                            "The QR code photo is in your gallerie",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        duration: Duration(seconds: 7),
+                        snackPosition: SnackPosition.TOP,
+                        margin: EdgeInsets.only(top: 100.0),
+                        backgroundColor: Colors.green,
+                        borderRadius: 20,
+                        isDismissible: true,
+                        dismissDirection: DismissDirection.horizontal,
+                        forwardAnimationCurve: Curves.easeOutBack,
+                      ));
+
+                      await DavinciCapture.click(imageKey,
+                          fileName: "QR code",
+                          saveToDevice: true,
+                          openFilePreview: true,
+                          albumName: 'QR code');
+                    },
+                    child: Text(
+                      "Take Picture",
+                    )),
+                Row(
+                  children: [
+                    Expanded(child: buildTextField(context)),
+                    const SizedBox(width: 12),
+                    FloatingActionButton(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      child: Icon(Icons.done, size: 30),
+                      onPressed: () => setState(() {}),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Widget buildTextField(BuildContext context) => TextField(
+        controller: controller,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+        decoration: InputDecoration(
+          hintText: 'Enter the data',
+          hintStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ),
+      );
+}
